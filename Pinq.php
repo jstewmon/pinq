3526740b13cb7bea57b9eb7d9a15c9407558a1a3
Jonathan Stewmon <jstewmon@gmail.com>
2012-07-27 10:12:15 -0500

    added lastFromSeekableCountable; formatted curly braces to be compliant

--- Pinq.php
+++ Pinq.php
@@ -1,9 +1,11 @@
 <?
 
-class DistinctIterator extends FilterIterator {
+class DistinctIterator extends FilterIterator
+{
   private $store;
   
-  public function __construct($iterator) {
+  public function __construct($iterator)
+  {
     if(!($iterator instanceof Iterator)) {
       $iterator = new ArrayIterator($iterator);
     }
@@ -11,7 +13,8 @@ class DistinctIterator extends FilterIterator {
     parent::__construct($iterator);
   }
 
-  public function accept() {
+  public function accept()
+  {
     $current = $this->current();
     if($this->store->offsetExists($current)) {
       return false;
@@ -24,10 +27,12 @@ class DistinctIterator extends FilterIterator {
 
 }
 
-class ExceptIterator extends FilterIterator {
+class ExceptIterator extends FilterIterator
+{
   private $exceptStore;
 
-  public function __construct($iterator, $except) {
+  public function __construct($iterator, $except)
+  {
 
     foreach($except as $value) {
       $this->exceptStore->detach($value);
@@ -35,7 +40,8 @@ class ExceptIterator extends FilterIterator {
     parent::__construct($iterator);
   }
 
-  public function accept() {
+  public function accept()
+  {
 
     if($this->exceptStore->offsetExists($current)) {
       return false;
@@ -44,11 +50,13 @@ class ExceptIterator extends FilterIterator {
   }
 }
 
-class WhereIterator extends FilterIterator {
+class WhereIterator extends FilterIterator
+{
 
   public $filter;
 
-  public function __construct($iterator, $filter) {
+  public function __construct($iterator, $filter)
+  {
     if(!is_callable($filter)) {
       throw new InvalidArgumentException('$filter should be callable');
     }
@@ -56,7 +64,8 @@ class WhereIterator extends FilterIterator {
     $this->filter = $filter;
   }
 
-  public function accept() {
+  public function accept()
+  {
     $iterator = $this->getInnerIterator();
     $key = $iterator->key();
     $current = $iterator->current();
@@ -75,39 +84,47 @@ class WhereIterator extends FilterIterator {
    maybe we can use IteratorIterator to wrap all the iterators we want to add from our
    deferred execution methods?
 */
-class PinqIterator implements OuterIterator {
+class PinqIterator implements OuterIterator
+{
 
   public $iterator;
 
-  function __construct($iterator = array()) {
+  function __construct($iterator = array())
+  {
     if($iterator instanceof Iterator) {
       $this->iterator = $iterator;
     }
     else $this->iterator = new ArrayIterator($iterator);
   }
 
-  public static function create($array = array()) {
+  public static function create($array = array())
+  {
     return new PinqIterator($array);
   }
 
-  public function aggregate($seed = null, $callback) {
+  public function aggregate($seed = null, $callback)
+  {
     return Pinq::aggregate($this->iterator, $seed, $callback);
   }
 
-  public function all($callback) {
+  public function all($callback)
+  {
     return Pinq::all($this, $callback);
   }
 
-  public function any($callback) {
+  public function any($callback)
+  {
     return Pinq::any($this, $callback);
   }
 
-  public function average($projection = null) {
+  public function average($projection = null)
+  {
     return Pinq::average($this, $projection);
   }
 
   // deferred
-  public function concat($iterator) {
+  public function concat($iterator)
+  {
     $itr = new AppendIterator();
     $itr->append($this->iterator);
     $itr->append($iterator);
@@ -115,115 +132,137 @@ class PinqIterator implements OuterIterator {
     return $this;
   }
 
-  public function contains($item) {
+  public function contains($item)
+  {
     return Pinq::contains($this, $item);
   }
 
-  public function containsKey($key) {
+  public function containsKey($key)
+  {
     return Pinq::containsKey($this, $key);
   }
 
-  public function count() {
+  public function count()
+  {
     return Pinq::count($this->iterator);
   }
 
   // deferred
-  public function distinct() {
+  public function distinct()
+  {
     $this->iterator = new DistinctIterator($this->iterator);
     return $this;
   }
 
   // deferred
-  public function except($array) {
+  public function except($array)
+  {
     $this->iterator = new ExceptIterator($this->iterator, $array);
     return $this;
   }
 
-  public function first($callback = null) {
+  public function first($callback = null)
+  {
     return Pinq::first($this->iterator, $callback);
   }
 
   // deferred
-  public function groupBy($keySelector, $valueSelector = null) {
+  public function groupBy($keySelector, $valueSelector = null)
+  {
 
     return $this;
   }
 
   // deferred
-  public function groupJoin() {
+  public function groupJoin()
+  {
 
     return $this;
   }
 
   // deferred
-  public function intersct($array) {
+  public function intersct($array)
+  {
 
     return $this;
   }
 
   // deferred
-  public function join() {
+  public function join()
+  {
     return $this;
   }
 
-  public function last($callback = null) {
+  public function last($callback = null)
+  {
     return Pinq::last($this->iterator, $callback);
   }
 
-  public function max($projection = null) {
+  public function max($projection = null)
+  {
 
   }
 
-  public function min($projection = null) {
+  public function min($projection = null)
+  {
 
   }
 
   // deferred
-  public function reverse() {
+  public function reverse()
+  {
     return $this;
   }
 
   // deferred
-  public function select($callback) {
-
-    return $this;
-  }
-
-  // deferred
-  public function selectMany() {
-
-    return $this;
-  }
-
-  // deferred
-  public function skip($howMany) {
+  public function select($callback)
+  {
 
     return $this;
   }
 
   // deferred
-  public function skipWhile($filter) {
-
-    return $this;
-  }
-
-  public function sum($projection = null) {
-
-  }
-
-  // deferred
-  public function take($howMany) {
+  public function selectMany()
+  {
 
     return $this;
   }
 
   // deferred
-  public function takeWhile($filter) {
+  public function skip($howMany)
+  {
+
+    return $this;
+  }
+
+  // deferred
+  public function skipWhile($filter)
+  {
+
+    return $this;
+  }
+
+  public function sum($projection = null)
+  {
+
+  }
+
+  // deferred
+  public function take($howMany)
+  {
+
+    return $this;
+  }
+
+  // deferred
+  public function takeWhile($filter)
+  {
     
     return $this;
   }
 
-  public function toArray() {
+  public function toArray()
+  {
 
     var_dump($this);
     $array = array();
@@ -234,58 +273,58 @@ class PinqIterator implements OuterIterator {
   }
 
   // deferred
-  public function union($array) {
+  public function union($iterator)
+  {
 
   }
 
   // deferred
-  public function where($callback) {
+  public function where($callback)
+  {
 
     $this->iterator = new WhereIterator($this->iterator, $callback);
     return $this;
   }
 
   // deferred
-  public function zip($array, $callback) {
+  public function zip($array, $callback)
+  {
 
     return $this;
   }
 
-  public function getInnerIterator() {
+  public function getInnerIterator()
+  {
 
     return $this->iterator;
   }
 
-  // private function stackCallback($callback) {
-  //   $self = $this;
-  //   $this->callback = function($key, $value, $iterator, $cb) use($self, $callback) {
-  //     // return $callback($self->callback($key, $value, $iterator));
-  //     return $self->callback($key, $value, $iterator, $callback);
-  //   };
-  //   return $this;
-  // }
-
   /* Iterator implementation */
-  public function current() {
+  public function current()
+  {
 
     return $this->iterator->current();
   }
 
-  public function key() {
+  public function key()
+  {
 
     return $this->iterator->key();
   }
 
-  public function next() {
+  public function next()
+  {
 
     return $this->iterator->next();
   }
-  public function rewind() {
+  public function rewind()
+  {
 
     var_dump($this);
     return $this->iterator->rewind();
   }
-  public function valid() {
+  public function valid()
+  {
 
     return $this->iterator->valid();
   }
@@ -295,9 +334,11 @@ class PinqIterator implements OuterIterator {
  * $callback is aways callable($key, $value)
  *
  */
-class Pinq {
+class Pinq
+{
 
-  public static function all($iterator, $callback) {
+  public static function all($iterator, $callback)
+  {
     foreach($iterator as $key => $value) {
       if(!callback($key, $value)) {
         return false;
@@ -306,7 +347,8 @@ class Pinq {
     return true;
   }
   
-  public static function any($iterator, $callback = false) {
+  public static function any($iterator, $callback = false)
+  {
     if(!$callback) return !empty($iterator);
     foreach($iterator as $key => $value) {
       if($callback($key, $value)) return true;
@@ -314,14 +356,16 @@ class Pinq {
     return false;
   }
 
-  public static function aggregate($iterator, $seed, $callback) {
+  public static function aggregate($iterator, $seed, $callback)
+  {
     foreach($iterator as $key => $value) {
       $seed = $callback($seed, $key, $value);
     }
     return $seed;
   }
 
-  public static function average($iterator, $callback = false) {
+  public static function average($iterator, $callback = false)
+  {
     $avg = 0;
     $len = count($iterator);
     foreach($iterator as $key => $value) {
@@ -333,22 +377,26 @@ class Pinq {
     return $avg;
   }
 
-  public static function contains($iterator, $item) {
+  public static function contains($iterator, $item)
+  {
     foreach($iterator as $value) {
       if($item == $value) return true;
     }
     return false;
   }
 
-  public static function count($iterator) {
+  public static function count($iterator)
+  {
     return count($iterator);
   }
 
-  public static function containsKey($iterator, $key) {
+  public static function containsKey($iterator, $key)
+  {
     return array_key_exists($key, $iterator);
   }
 
-  public static function first($iterator, $callback = null) {
+  public static function first($iterator, $callback = null)
+  {
     self::ensureCallback($callback, true);
 
     foreach($iterator as $key => $value) {
@@ -360,8 +408,13 @@ class Pinq {
     return null;
   }
 
-  public static function last($iterator, $callback = null) {
+  public static function last($iterator, $callback = null)
+  {
     self::ensureCallback($callback, true);
+
+    if($iterator instanceof SeekableIterator && $iterator instanceof Countable) {
+      return self::lastFromSeekableCountable($iterator, $callback);
+    }
 
     $array = array();
     if(!is_array($iterator)) {
@@ -373,8 +426,25 @@ class Pinq {
     return self::lastFromArray($array, $callback);
   }
 
-  private static function lastFromArray($iterator, $callback = null) {
-    self::ensureCallback($callback, true);
+  private static function lastFromSeekableCountable($iterator, $callback)
+  {
+    $position = $iterator->count() - 1;
+    do {
+      $iterator->seek($position);
+      $key = $iterator->key();
+      $value = $iterator->current();
+      if($callback == null) {
+        return array($key, $value, 'key' => $key, 'value' => $value);
+      }
+      if($callback($key, $value)) {
+        return array($key, $value, 'key' => $key, 'value' => $value);
+      }
+    }
+    while ($position-- > 0);
+  }
+
+  private static function lastFromArray($iterator, $callback = null)
+  {
     
     $value = end($iterator);
     $key = key($iterator);
@@ -382,28 +452,32 @@ class Pinq {
       if($callback == null) {
         return array($key, $value, 'key' => $key, 'value' => $value);
       }
-      if($callback($key, $value, $iterator)) {
+      if($callback($key, $value)) {
         return array($key, $value, 'key' => $key, 'value' => $value);
       }
     }
     while($value = prev($iterator)); 
   }
 
-  public static function where($iterator, $callback) {
+  public static function where($iterator, $callback)
+  {
     return PinqIterator::create($iterator)->where($callback);
   }
 
-  public static function repeat($value, $times) {
+  public static function repeat($value, $times)
+  {
 
   }
 
-  public static function ensureIterator($iterator) {
+  public static function ensureIterator($iterator)
+  {
     if($iterator instanceof Traversable) return;
     if(is_array($iterator)) return;
     throw new InvalidArgumentException('$iterator must be either an array or an instance of Traversable');
   }
 
-  public static function ensureCallback($callback, $allowNull = false) {
+  public static function ensureCallback($callback, $allowNull = false)
+  {
     if($callback == null && $allowNull) {
       return;
     }
